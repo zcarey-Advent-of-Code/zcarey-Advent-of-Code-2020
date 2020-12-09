1f809eef5f3d8fb35bbc401decc1ea75e0c6be1b
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2020-12-09 00:20:42 -0500

    Finished part 2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -6,27 +6,35 @@ using System.Linq;
 namespace Day09 {
 	class Program : ParsedInputProgramStructure<long> {
 
+		private const int PREAMBLE_LEN = 25;
+		//private const int PREAMBLE_LEN = 5;
+
 		Program() : base(long.Parse) {
 		}
 
 		static void Main(string[] args) {
 			new Program().Run("Input.txt");
+			//new Program().Run("Example.txt");
 		}
 
-		protected override string CalculatePart1(long[] input) {
+		private static long calcPart1(long[] input) {
 			List<long> preamble = new List<long>();
-			for(int i = 0; i < 25; i++) {
+			for (int i = 0; i < PREAMBLE_LEN; i++) {
 				preamble.Add(input[i]);
 			}
-			foreach(long data in input.Skip(25)) {
-				if(!ValidXMas(preamble, data)) {
-					return data.ToString();
+			foreach (long data in input.Skip(PREAMBLE_LEN)) {
+				if (!ValidXMas(preamble, data)) {
+					return data;
 				} else {
 					preamble.RemoveAt(0);
 					preamble.Add(data);
 				}
 			}
 			throw new Exception("Could not compute.");
+		}
+
+		protected override string CalculatePart1(long[] input) {
+			return calcPart1(input).ToString();
 		}
 
 		static bool ValidXMas(List<long> preamble, long data) {
@@ -41,7 +49,31 @@ namespace Day09 {
 		}
 
 		protected override string CalculatePart2(long[] input) {
-			return "null";
+			long target = calcPart1(input);
+			for (int i = 0; i < input.Length - 1; i++) {
+				for(int j = i + 1; j < input.Length; j++) {
+					long result = SumRange(input, i, j);
+					if(result == target) {
+						return AddMinMax(input, i, j).ToString();
+					}else if(result > target) {
+						break;
+					}
+				}
+			}
+			throw new Exception("Could not compute.");
+		}
+
+		static long SumRange(long[] input, int i, int j) {
+			long count = 0;
+			for(; i <= j; i++) {
+				count += input[i];
+			}
+			return count;
+		}
+
+		static long AddMinMax(long[] input, int i, int j) {
+			IEnumerable<long> data = input.Skip(i).Take(j - i);
+			return data.Min() + data.Max();
 		}
 	}
 }
