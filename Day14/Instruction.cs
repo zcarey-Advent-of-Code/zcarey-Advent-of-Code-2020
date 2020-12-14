e1635552b77d70f7072ba04636c8a9fea4ca24bc
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2020-12-14 08:35:16 -0500

    Day 14 Part 1 done

---
+++ Day14/Instruction.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14 {
+	abstract class Instruction {
+
+		public abstract void Update(long[] memory, ref long mask, ref long maskSet);
+
+		public static Instruction Parse(string input) {
+			Instruction result = null;
+			if((result = Mask.TryParse(input)) == null) {
+				if((result = Memory.TryParse(input)) == null) {
+					throw new Exception("Unable to parse input.");
+				}
+			}
+			return result;
+		}
+
+	}
+
+	class Mask : Instruction {
+
+		long mask = 0;
+		long maskSet = 0;
+
+		private Mask() { }
+
+		public override void Update(long[] memory, ref long mask, ref long maskSet) {
+			mask = this.mask;
+			maskSet = this.maskSet;
+		}
+
+		public static Instruction TryParse(string input) {
+			if (input.StartsWith("mask")) {
+				string value = input.Substring(7);
+				Mask result = new Mask();
+				foreach(char c in value) {
+					result.mask <<= 1;
+					result.maskSet <<= 1;
+					if(c == 'X') {
+						result.mask |= 1;
+					}else if(c == '1') {
+						result.maskSet |= 1;
+					}else if(c != '0') {
+						throw new Exception("Bad input.");
+					}
+				}
+				return result;
+			} else {
+				return null;
+			}
+		}
+	}
+
+	class Memory : Instruction {
+
+		int index = 0;
+		long value = 0;
+
+		private Memory() { }
+
+		public override void Update(long[] memory, ref long mask, ref long maskSet) {
+			memory[index] = (this.value & mask) | maskSet;
+		}
+
+		public static Instruction TryParse(string input) {
+			if (input.StartsWith("mem[")) {
+				Memory result = new Memory();
+				result.index = int.Parse(input.Substring(4, input.IndexOf(']') - 4));
+				result.value = long.Parse(input.Substring(input.IndexOf('=') + 1));
+				return result;
+			} else {
+				return null;
+			}
+		}
+	}
+}
---
+++ Day14/Program.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using System.Linq;
+
+namespace Day14 {
+	class Program : ParsedInputProgramStructure<Instruction> {
+
+		Program() : base(x => Instruction.Parse(x)) {
+		}
+
+		static void Main(string[] args) {
+			new Program().Run("Input.txt");
+		}
+
+		protected override string CalculatePart1(Instruction[] input) {
+			long[] memory = new long[70000];
+			long mask = 0;
+			long maskSet = 0;
+			foreach (Instruction op in input) {
+				op.Update(memory, ref mask, ref maskSet);
+			}
+			return (memory.Sum()).ToString();
+		}
+
+		protected override string CalculatePart2(Instruction[] input) {
+			return "null";
+		}
+	}
+}
