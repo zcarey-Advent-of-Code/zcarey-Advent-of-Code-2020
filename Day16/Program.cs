09146a38f857e4a65bbf3c478adee5f1fa65dc5f
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2020-12-16 10:58:30 -0500

    Day 16 part 2 done

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -31,7 +31,66 @@ namespace Day16 {
 		}
 
 		protected override string CalculatePart2(InputData input) {
-			return "null";
+			//Initialize all valid rules
+			List<Rule>[] validRules = new List<Rule>[input.TicketLength];
+			for(int i = 0; i < validRules.Length; i++) {
+				validRules[i] = new List<Rule>(input.Rules);
+			}
+
+			//Determine valid rules by checking against valid tickets
+			foreach(int[] ticket in GetValidTickets(input)) {
+				for(int i = 0; i < ticket.Length; i++) {
+					validRules[i].RemoveAll(rule => !rule.FitsInRange(ticket[i]));
+				}
+			}
+
+			//Keep eliminating rules by finding fields that only have 1 validm rule and remove that rule from all other fields
+			bool edited = true;
+			while (edited) {
+				edited = false;
+				foreach(List<Rule> rules in validRules) {
+					if(rules.Count == 1) {
+						Rule confirmedRule = rules[0];
+						foreach(List<Rule> otherRules in validRules.Where(x => x != rules)) {
+							edited |= otherRules.Remove(confirmedRule);
+						}
+					}
+				}
+			}
+
+			//Assert that we found them all correctly!
+			foreach(List<Rule> rules in validRules) {
+				if(rules.Count != 1) {
+					throw new Exception("Could not determine rules!");
+				}
+			}
+
+			//Calcualte answer
+			long result = 1;
+			for(int i = 0; i < input.MyTicket.Length; i++) {
+				Rule rule = validRules[i][0];
+				int myTicketNumber = input.MyTicket[i];
+				if (rule.Key.StartsWith("departure")) {
+					result *= myTicketNumber;
+				}
+			}
+
+			return result.ToString();
+		}
+
+		private IEnumerable<int[]> GetValidTickets(InputData input) {
+			foreach(int[] ticket in input.OtherTickets) {
+				bool validTicket = true;
+				foreach(int number in ticket) {
+					if(!FindValidRules(input, number).Any()) {
+						validTicket = false;
+						break;
+					}
+				}
+				if (validTicket) {
+					yield return ticket;
+				}
+			}
 		}
 	}
 }
