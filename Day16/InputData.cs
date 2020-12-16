b8999f1f49dcbc38852693f4e4c490375a67979a
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2020-12-16 10:39:22 -0500

    Day 16 part 1 done

---
+++ Day16/InputData.cs
@@ -0,0 +1,42 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Day16 {
+	class InputData : FullInputParser {
+
+		public Rule[] Rules { get; private set; }
+		public int TicketLength { get; private set; }
+		public int[] MyTicket { get; private set; }
+		public int[][] OtherTickets { get; private set; }
+
+		public void Parse(string[] input) {
+			IEnumerable<IEnumerable<string>> blocks = input.GetGroups();
+			IEnumerator<IEnumerable<string>> enumerator = blocks.GetEnumerator();
+			enumerator.MoveNext();
+			parseRules(enumerator.Current);
+			enumerator.MoveNext();
+			parseMyTicket(enumerator.Current);
+			enumerator.MoveNext();
+			parseOtherTickets(enumerator.Current);
+
+			TicketLength = MyTicket.Length;
+		}
+
+		private void parseRules(IEnumerable<string> input) {
+			this.Rules = input.Select(x => new Rule(x)).ToArray();
+		}
+
+		private void parseMyTicket(IEnumerable<string> input) {
+			if (!input.First().StartsWith("your ticket:")) throw new Exception("Bad input.");
+			MyTicket = input.Skip(1).First().Split(',').Select(int.Parse).ToArray();
+		}
+
+		private void parseOtherTickets(IEnumerable<string> input) {
+			if (!input.First().StartsWith("nearby tickets:")) throw new Exception("Bad input.");
+			OtherTickets = input.Skip(1).Select(x => x.Split(',').Select(int.Parse).ToArray()).ToArray();
+		}
+	}
+}
---
+++ Day16/Program.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day16 {
+	class Program : FullParsedInputProgramStructure<InputData> {
+		static void Main(string[] args) {
+			new Program().Run("input.txt");
+			//new Program().Run("example.txt");
+		}
+
+		protected override string CalculatePart1(InputData input) {
+			int ticketScanningErrorRate = 0;
+			foreach(int[] ticket in input.OtherTickets) {
+				foreach(int number in ticket) {
+					if(!FindValidRules(input, number).Any()) {
+						ticketScanningErrorRate += number;
+					}
+				}
+			}
+			return ticketScanningErrorRate.ToString();
+		}
+
+		private IEnumerable<Rule> FindValidRules(InputData input, int num) {
+			foreach(Rule rule in input.Rules) {
+				if (rule.FitsInRange(num)) {
+					yield return rule;
+				}
+			}
+		}
+
+		protected override string CalculatePart2(InputData input) {
+			return "null";
+		}
+	}
+}
