7758a9d91792cdf9114b21b7a768f4f9449e6d04
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2020-12-18 21:43:01 -0500

    Day 18 Part 1 done

---
+++ Day18/Expression.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Day18 {
+	//Definitely not the best way to do this, but it works
+	class Expression {
+
+		private BigInteger result = 0;
+		private List<Expression> parameters = new List<Expression>();
+		private List<Operator> operators = new List<Operator>();
+
+		private Expression() { }
+		private Expression(int constant) {
+			this.result = constant;
+		}
+
+		public BigInteger Calculate() {
+			if (parameters.Count == 0 && operators.Count == 0) return result;
+
+			while(operators.Count > 0) {
+				Reduce();
+			}
+			if (parameters.Count != 1) throw new Exception("Invalid state: there is no result.");
+			result = parameters[0].Calculate();
+			parameters.Clear();
+			return result;
+		}
+
+		private void Reduce() {
+			int operatorIndex = 0; // operators.IndexOf(Operator.Multiply);
+			/*if (operatorIndex < 0) {
+				operatorIndex = operators.IndexOf(Operator.Add);
+				if(operatorIndex < 0) {
+					throw new Exception("Invalid operator.");
+				}
+			}*/
+			int param1Index = operatorIndex;
+			int param2Index = param1Index + 1;
+			BigInteger result = operators[operatorIndex].Calculate(parameters[param1Index].Calculate(), parameters[param2Index].Calculate());
+			parameters.RemoveAt(param2Index);
+			operators.RemoveAt(operatorIndex);
+			parameters[param1Index].result = result;
+		}
+
+		public static Expression Parse(string input) {
+			int index = 0;
+			Expression exp = parseExpression(input, ref index);
+			if (index < input.Length) throw new Exception("Parse ended too early.");
+			return exp;
+		}
+
+		private static Expression parseExpression(string input, ref int index) {
+			Expression exp = new Expression();
+			exp.parameters.Add(parseParameter(input, ref index));
+			while ((index < input.Length) && (input[index] != ')')) {
+				exp.operators.Add(parseOperator(input, ref index));
+				exp.parameters.Add(parseParameter(input, ref index));
+			}
+			return exp;
+		}
+
+		private static Expression parseParameter(string expression, ref int index) {
+			skipWhitespace(expression, ref index);
+			if (expression[index] == '(') {
+				index++;
+				Expression exp = parseExpression(expression, ref index);
+				index++; //Move past the closing bracket ')'
+				return exp;
+			} else {
+				int indexStart = index;
+				while ((index < expression.Length) && char.IsNumber(expression[index])) {
+					index++;
+				}
+				return new Expression(int.Parse(expression.Substring(indexStart, index - indexStart)));
+			}
+		}
+		private static Operator parseOperator(string input, ref int index) {
+			skipWhitespace(input, ref index);
+			switch (input[index++]) {
+				case '+': return Operator.Add;
+				case '*': return Operator.Multiply;
+				default: throw new Exception("Invalid operator.");
+			}
+		}
+
+		private static void skipWhitespace(string expression, ref int index) {
+			while (char.IsWhiteSpace(expression[index])) {
+				index++;
+			}
+		}
+
+	}
+}
---
+++ Day18/Program.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using System.Linq;
+using System.Numerics;
+
+namespace Day18 {
+	class Program : ParsedInputProgramStructure<Expression> {
+
+
+		Program() : base(Expression.Parse) {
+		}
+
+		static void Main(string[] args) {
+			new Program().Run("input.txt");
+			//new Program().Run("Example26.txt");
+			//new Program().Run("Example51.txt");
+			//new Program().Run("Example71.txt");
+			//new Program().Run("Example437.txt");
+			//new Program().Run("Example12240.txt");
+			//new Program().Run("Example13632.txt");
+		}
+
+		protected override string CalculatePart1(Expression[] input) {
+			BigInteger result = BigInteger.Zero;
+			foreach(BigInteger answer in input.Select(x => x.Calculate())) {
+				result += answer;
+			}
+			return result.ToString();
+		}
+
+		protected override string CalculatePart2(Expression[] input) {
+			return "null";
+		}
+	}
+}
