8cac107841c2fd47a53ff52db52e65f977a0dfa1
Zachary Carey <zach.carey@outlook.com>
2020-12-24 01:16:07 -0500

    ok time for bed

--- Day20/Map.cs
+++ Day20/Map.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Text;
+using System.Linq;
 
 namespace Day20 {
 	class Map {
@@ -32,8 +32,100 @@ namespace Day20 {
 		}
 
 		public bool ValidTileLocation(Point loc, Tile tile) {
+			if (this[loc] != null) return false;
 
+			//Check Up
+			Tile up = this[loc.X, loc.Y - 1];
+			if (up != null && !tile.Top.SequenceEqual(up.Bottom)) return false;
+
+			//Check Down
+			Tile down = this[loc.X, loc.Y + 1];
+			if (down != null && !tile.Bottom.SequenceEqual(down.Top)) return false;
+
+			//Check Left
+			Tile left = this[loc.X - 1, loc.Y];
+			if (left != null && !tile.Left.SequenceEqual(left.Right)) return false;
+
+			//Check Right
+			Tile right = this[loc.X + 1, loc.Y];
+			if (right != null && !tile.Right.SequenceEqual(right.Left)) return false;
+
+			return true;
 		}
 
+		internal Rectangle GetBounds() {
+			IEnumerable<Point> tiles = map.Where(x => x.Value != null).Select(x => x.Key);
+			Point first = tiles.First();
+			int left = first.X;
+			int right = first.X;
+			int top = first.Y;
+			int bottom = first.Y;
+			foreach(Point tile in tiles.Skip(1)) {
+				left = Math.Min(left, tile.X);
+				right = Math.Max(right, tile.X);
+				top = Math.Min(top, tile.Y);
+				bottom = Math.Max(bottom, tile.Y);
+			}
+			return new Rectangle(left, top, right - left, bottom - top);
+		}
+
+		internal bool TilesInASquare() {
+			IEnumerable<Point> tiles = map.Where(x => x.Value != null).Select(x => x.Key);
+
+			//Find the left and right of the first row
+			Point first = tiles.First();
+			int left = first.X;
+			int right = first.X;
+			while(this[left - 1, first.Y] != null) {
+				left = left - 1;
+			}
+			while (this[left + 1, first.Y] != null) {
+				right = right + 1;
+			}
+
+			//Check rows above
+			for(int y = first.Y - 1; ; y--) {
+				bool allTilesPresent = true;
+				bool atLeastOneTileFound = false;
+				if ((this[left - 1, y] != null) || (this[right + 1, y] != null)) return false;
+				for(int x = left; x <= right; x++) {
+					if(this[x, y] != null) {
+						atLeastOneTileFound = true;
+					} else {
+						allTilesPresent = false;
+					}
+				}
+				if (!allTilesPresent) {
+					if (atLeastOneTileFound) {
+						return false;
+					} else {
+						break;
+					}
+				}
+			}
+
+			//Check rows below
+			for (int y = first.Y + 1; ; y++) {
+				bool allTilesPresent = true;
+				bool atLeastOneTileFound = false;
+				if ((this[left - 1, y] != null) || (this[right + 1, y] != null)) return false;
+				for (int x = left; x <= right; x++) {
+					if (this[x, y] != null) {
+						atLeastOneTileFound = true;
+					} else {
+						allTilesPresent = false;
+					}
+				}
+				if (!allTilesPresent) {
+					if (atLeastOneTileFound) {
+						return false;
+					} else {
+						break;
+					}
+				}
+			}
+
+			return true;
+		}
 	}
 }
--- Day20/Program.cs
+++ Day20/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 
 namespace Day20 {
 	class Program : BlockParsedInputProgramStructure<Tile> {
@@ -24,10 +25,51 @@ namespace Day20 {
 
 		protected override string CalculatePart1(Tile[] input) {
 			Map map = new Map();
-			
+			if(!placeTile(map, input, 0, new Point())) {
+				throw new Exception("Could not solve!");
+			}
+			Rectangle bounds = map.GetBounds();
+			BigInteger idTL = map[bounds.Left, bounds.Top].ID;
+			BigInteger idTR = map[bounds.Right, bounds.Top].ID;
+			BigInteger idBR = map[bounds.Right, bounds.Bottom].ID;
+			BigInteger idBL = map[bounds.Left, bounds.Bottom].ID;
+			return (idTL * idTR * idBR * idBL).ToString();
 		}
 
-		private bool placeTime()
+		private bool placeTile(Map map, Tile[] tiles, int index, Point previousLocation) {
+			if (index >= tiles.Length) {
+				//Base case
+				return map.TilesInASquare();
+			}
+
+			Tile tile = tiles[index];
+
+			//Try up
+			if (TryPlaceTile(map, tiles, index, tile, new Point(previousLocation.X, previousLocation.Y - 1))) return true;
+
+			//Try down
+			if (TryPlaceTile(map, tiles, index, tile, new Point(previousLocation.X, previousLocation.Y + 1))) return true;
+
+			//Try left
+			if (TryPlaceTile(map, tiles, index, tile, new Point(previousLocation.X - 1, previousLocation.Y))) return true;
+
+			//Try right
+			if (TryPlaceTile(map, tiles, index, tile, new Point(previousLocation.X + 1, previousLocation.Y))) return true;
+
+			return false;
+		}
+
+		private bool TryPlaceTile(Map map, Tile[] tiles, int index, Tile tile, Point loc) {
+			foreach (Operation op in ValidOperations) {
+				tile.Operation = op;
+				if (map.ValidTileLocation(loc, tile)) {
+					map[loc] = tile;
+					if (placeTile(map, tiles, index + 1, loc)) return true;
+					map[loc] = null;
+				}
+			}
+			return false;
+		}
 
 		protected override string CalculatePart2(Tile[] input) {
 			throw new NotImplementedException();
