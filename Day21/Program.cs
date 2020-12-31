9df3a79ec1f1cf63207b69949a53210b6c606de7
Zachary Carey <zach.carey@outlook.com>
2020-12-31 11:30:58 -0500

    Added and verified input parsing

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -1,9 +1,26 @@
-﻿using System;
+﻿using Common;
+using System;
 
 namespace Day21 {
-	class Program {
+	class Program : ParsedInputProgramStructure<Food> {
+
+		Program() : base(Food.Parse) {
+		}
+
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			//new Program().Run("input.txt");
+			new Program().Run("Example.txt");
+		}
+
+		protected override string CalculatePart1(Food[] input) {
+			foreach(Food food in input) {
+				Console.WriteLine(food);
+			}
+			return "null";
+		}
+
+		protected override string CalculatePart2(Food[] input) {
+			return "null";
 		}
 	}
 }
