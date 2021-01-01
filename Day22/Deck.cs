44cd233efd2142948d6f9a80e437c656a5a5669e
Zachary Carey <zach.carey@outlook.com>
2021-01-01 11:32:50 -0500

    Day 22 input parsed and verified

---
+++ Day22/Deck.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day22 {
+	class Deck : FullInputParser {
+
+		public int Player { get; private set; }
+		public IEnumerable<int> Cards { get => cards; }
+
+		private List<int> cards;
+
+		public void Parse(string[] input) {
+			this.Player = int.Parse(input[0].Substring("Player ".Length, 1));
+			this.cards = input.Skip(1).Select(int.Parse).ToList();
+		}
+
+		public override string ToString() {
+			return string.Join(", ", this.cards);
+		}
+	}
+}
---
+++ Day22/Program.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using System;
+
+namespace Day22 {
+	class Program : BlockParsedInputProgramStructure<Deck> {
+
+		static void Main(string[] args) {
+			//new Program().Run("input.txt");
+			new Program().Run("Example.txt");
+		}
+
+		protected override string CalculatePart1(Deck[] input) {
+			Console.WriteLine("Player {1}:\n{0}", input[0], input[0].Player);
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Player {1}:\n{0}", input[1], input[1].Player);
+			return "null";
+		}
+
+		protected override string CalculatePart2(Deck[] input) {
+			return "null";
+		}
+	}
+}
