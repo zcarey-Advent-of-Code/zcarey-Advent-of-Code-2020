59811386892c110871f6afe3e13de272a160ff85
Zachary Carey <zach.carey@outlook.com>
2021-01-01 11:08:59 -0500

    Day 21 part 1 done

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -10,33 +10,40 @@ namespace Day21 {
 		}
 
 		static void Main(string[] args) {
-			//new Program().Run("input.txt");
-			new Program().Run("Example.txt");
+			new Program().Run("input.txt");
+			//new Program().Run("Example.txt");
 		}
 
 		protected override string CalculatePart1(Food[] input) {
-			//Get a list of all possible allergens
-			List<string> allergens = input.SelectMany(x => x.Allergens).Distinct().ToList();
+			List<string> foundAllergens = new List<string>();
+			Dictionary<string, List<string>> allergens = new Dictionary<string, List<string>>();
 
-			//The initial state each ingredient can be any of the possible allergens
-			Dictionary<string, List<string>> possibleAllergens = new Dictionary<string, List<string>>();
-			foreach(string ingredient in input.SelectMany(x => x.Ingredients).Distinct()) {
-				possibleAllergens[ingredient] = new List<string>(allergens);
-			}
+			bool changed = true;
+			while (changed) {
+				changed = false;
 
-			//Now using our input, elimate allergens that arent possible for some ingredients
-			foreach(Food food in input) {
-				List<string> missingAllergens = allergens.Except(food.Allergens).ToList();
-				foreach(string ingredient in food.Ingredients) {
-					foreach (string allergen in missingAllergens) {
-						possibleAllergens[ingredient].Remove(allergen); //It can't be an allergen that isn't in this food's allergen list
+				foreach(Food food in input) {
+					foreach(string allergen in food.Allergens) {
+						if (!allergens.ContainsKey(allergen)) {
+							allergens[allergen] = new List<string>(food.Ingredients);
+							changed = true;
+						} else {
+							List<string> newList = allergens[allergen].Intersect(food.Ingredients).ToList();
+							changed |= (newList.Count != allergens[allergen].Count);
+							allergens[allergen] = newList;
+						}
+
+						changed |= (allergens[allergen].RemoveAll(x => foundAllergens.Contains(x)) != 0);
+						if (allergens[allergen].Count == 1 && !foundAllergens.Contains(allergens[allergen][0])) {
+							foundAllergens.Add(allergens[allergen][0]);
+							changed = true;
+						}
 					}
 				}
 			}
 
-			//Get a list of all ingredients with no allergens and count how many of them appear in our input
-			List<string> noAllergens = possibleAllergens.Where(pair => pair.Value.Count == 0).Select(pair => pair.Key).ToList();
-			return input.SelectMany(x => x.Ingredients).Where(x => noAllergens.Contains(x)).Count().ToString();
+			//The "Except" function in linq is for some reason removing duplicate elements so we have to do it the ugly way instead.
+			return input.SelectMany(x => x.Ingredients).Where(x => !foundAllergens.Contains(x)).Count().ToString(); 
 		}
 
 		protected override string CalculatePart2(Food[] input) {
