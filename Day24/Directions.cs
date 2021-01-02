31dde650415ee9080715a35c9e17a37b96a9e247
Zachary Carey <zach.carey@outlook.com>
2021-01-02 12:29:08 -0500

    Day 24 part 1 done

---
+++ Day24/Directions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Day24 {
+	class Directions /*: IEnumerable<Point>*/ {
+
+		private Point[] offsets;
+
+		public Directions(string input) {
+			offsets = convert(input).ToArray();
+		}
+
+		private IEnumerable<Point> convert(string input) {
+			IEnumerator<char> chars = input.GetEnumerator();
+			while (chars.MoveNext()) {
+				switch (chars.Current) {
+					case 'w': 
+						yield return new Point(-1, 0);
+						break;
+					case 'e':
+						yield return new Point(1, 0);
+						break;
+					case 'n':
+						if (!chars.MoveNext()) throw new ArgumentOutOfRangeException("input", "Bad input, out of characters.");
+						yield return new Point((chars.Current == 'e') ? 1 : 0, -1);
+						break;
+					case 's':
+						if (!chars.MoveNext()) throw new ArgumentOutOfRangeException("input", "Bad input, out of characters.");
+						yield return new Point((chars.Current == 'w') ? -1 : 0, 1);
+						break;
+					default:
+						throw new ArgumentOutOfRangeException("input", "Bad input.");
+				}
+			}
+		}
+
+		public Point GetAbsoluteOffset(Point origin = new Point()) {
+			foreach(Point offset in offsets) {
+				origin.Offset(offset);
+			}
+			return origin;
+		}
+		/*
+		public IEnumerator<Point> GetEnumerator() {
+			return this.offsets.GetEnumerator<Point>(); //new CustomEnumerator(offsets);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return this.GetEnumerator();
+		}
+		*/
+		public static Directions Parse(string input) {
+			return new Directions(input);
+		}
+/*
+		private class CustomEnumerator : IEnumerator<(int q, int r)> {
+			public (int q, int r) Current => array[index];
+			object IEnumerator.Current => this.Current;
+
+			private (int q, int r)[] array;
+			int index = -1;
+
+			public CustomEnumerator((int q, int r)[] array) {
+				this.array = array;
+			}
+
+			public void Dispose() {
+				this.array = null;
+			}
+
+			public bool MoveNext() {
+				index++;
+				return index < array.Length;
+			}
+
+			public void Reset() {
+				this.index = -1;
+			}
+		}
+*/	}
+}
--- Day24/Program.cs
+++ Day24/Program.cs
@@ -1,9 +1,33 @@
-﻿using System;
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
 namespace Day24 {
-	class Program {
+	class Program : ParsedInputProgramStructure<Directions> {
+
+		Program() : base(Directions.Parse) {
+		}
+
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			new Program().Run("input.txt");
+			//new Program().Run("Example.txt");
+		}
+
+		protected override string CalculatePart1(Directions[] input) {
+			Dictionary<Point, bool> grid = new Dictionary<Point, bool>();
+			foreach(Directions directions in input) {
+				Point result = directions.GetAbsoluteOffset();
+				if (!grid.ContainsKey(result)) grid[result] = false; //Because tiles are by-default white, so now that it's flipped it is black
+				else grid[result] ^= true;
+			}
+
+			return grid.Select(pair => pair.Value).Count(x => x == false).ToString();
+		}
+
+		protected override string CalculatePart2(Directions[] input) {
+			return "null";
 		}
 	}
 }
