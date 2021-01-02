dff3e197bb432fc844d0f24b33f791d2986bc2c7
Zachary Carey <zach.carey@outlook.com>
2021-01-02 14:52:10 -0500

    Day 25 part 1 done

---
+++ Day25/Program.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using System;
+
+namespace Day25 {
+	class Program : ParsedInputProgramStructure<PublicKey> {
+
+		Program() : base(x => new PublicKey(x)) { }
+
+		static void Main(string[] args) {
+			new Program().Run("input.txt");
+			//new Program().Run("Example.txt");
+		}
+
+		protected override string CalculatePart1(PublicKey[] input) {
+			PublicKey cardPublicKey = input[0];
+			PublicKey doorPublicKey = input[1];
+
+			int cardLoopSize = cardPublicKey.FindLoopSize(7);
+			int doorLoopSize = doorPublicKey.FindLoopSize(7);
+
+			long encryptionKey = PublicKey.Transform(doorPublicKey.Key, cardLoopSize);
+			return encryptionKey.ToString();
+		}
+
+		protected override string CalculatePart2(PublicKey[] input) {
+			return "null";
+		}
+	}
+}
